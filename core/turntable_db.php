a1c6b501a5e594554acb6c40b1f320a1f21d50e2
Paul Vorbach <paul@vorba.ch>
2014-05-26 18:03:39 +0200

    Database wrapper

---
+++ core/turntable_db.php
@@ -0,0 +1,52 @@
+<?php
+require_once '../turntable_master.php';
+
+abstract class turntable_db {
+  private $connection;
+  private $author;
+  private $prefix;
+
+  /**
+   * Creates a new connection to the database.
+   *
+   * @param string $host
+   *          DB host and optional port
+   * @param string $user
+   *          DB user name
+   * @param string $password
+   *          DB user password
+   * @param string $database
+   *          Database in MySQL installation
+   * @param string $author
+   *          DB author
+   * @param string $prefix
+   *          optional prefix string
+   */
+  public function __construct($host, $user, $password, $database, $author = 'UNKNOWN', $prefix = '') {
+    $port = 3306; // default port
+
+    // host may also contain port separated with ':'
+    if (strpos($host, ':') >= 0) {
+      $parts = explode(':', $host);
+      $host = $parts[0];
+      if (isset($parts[1])) {
+        $port = $parts[1];
+      }
+    }
+
+    // connect to the db and store the connection
+    $this->connection = new mysqli($host, $user, $password, $database, $port);
+    $this->connection->set_charset('utf8');
+
+    // remember author and prefix
+    $this->author = $author;
+    $this->prefix = $prefix;
+  }
+
+  /**
+   * Closes the DB connection.
+   */
+  public function __destruct() {
+    $this->connection->close();
+  }
+}
---
+++ core/turntable_master_db.php
@@ -0,0 +1,11 @@
+<?php
+require_once './turntable_db.php';
+require_once '../turntable_master.php';
+
+/**
+ * Master database wrapper.
+ *
+ * @author Paul Vorbach
+ */
+class turntable_master_db extends turntable_db {
+}
