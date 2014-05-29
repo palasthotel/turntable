8681c8aaffea575b1360fe822179c6f9028a7a78
Paul Vorbach <paul@vorba.ch>
2014-05-29 16:41:23 +0200

    Remove setSharedState()

--- turntable_client.php
+++ turntable_client.php
@@ -74,12 +74,4 @@ class turntable_client {
   public function uninstall() {
     // TODO what to do here?
   }
-
-  public function setSharedState($nid, $shared_state) {
-    $query = 'INSERT INTO `' . $this->prefix . self::TABLE_CLIENT_INFO .
-         '` (`nid`, `shared_state`) VALUES (' . $nid . ',' . $shared_state .
-         ') ON DUPLICATE KEY UPDATE `shared_state`=' . $shared_state . ';';
-
-    $result = $this->getDB()->getConnection()->query($query);
-  }
 }
