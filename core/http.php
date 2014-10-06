7768733fe74f11844c6e1ed5c43546c8a249751e
Paul Vorbach <paul@vorba.ch>
2014-10-06 10:31:30 +0200

    File download and upload (untested)

--- core/http.php
+++ core/http.php
@@ -29,3 +29,25 @@ function http_req($method, $url, $headers = array(), $data = '') {
 
   return $response;
 }
+
+/**
+ * Downloads the file/resource at $source_url to the file at $destination_path.
+ *
+ * @param string $source_url
+ * @param string $destination_path
+ */
+function http_file_download($source_url, $destination_path) {
+  file_put_contents($destination_path, fopen($source_url, 'r'));
+}
+
+function http_file_upload($source_file, $destination_url) {
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS,
+      array(
+        'file' => '@' . $source_file
+      ));
+  curl_setopt($curl, CURLOPT_URL, $destination_url);
+  curl_exec($curl);
+  curl_close($curl);
+}
