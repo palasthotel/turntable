c4849aed5f5a9ac1d1d4b0a7befa2032528772b2
Paul Vorbach <paul@vorba.ch>
2014-10-08 17:44:57 +0200

    turntable_db_client::setSharedStateIgnoreExisting()

--- core/turntable_db.php
+++ core/turntable_db.php
@@ -185,6 +185,21 @@ SQL;
     return $this->connection->query($sql);
   }
 
+  public function setSharedStateIgnoreExisting($nid, $shared_state) {
+    $table = $this->prefix . self::TABLE_NODE_SHARED;
+
+    $sql = <<<SQL
+INSERT IGNORE INTO $table
+  (nid, shared_state)
+VALUES
+  ($nid, $shared_state);
+SQL;
+
+    $res = $this->connection->query($sql);
+
+    return $res;
+  }
+
   public function setSharedState($nid, $shared_state) {
     $table = $this->prefix . self::TABLE_NODE_SHARED;
 
