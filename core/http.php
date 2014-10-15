f7b4a6e9d6bab99012c8082bb36ec597989a0c7d
Paul Vorbach <paul@vorba.ch>
2014-10-15 17:36:17 +0200

    use cURL instead of file_get_contents()

--- core/http.php
+++ core/http.php
@@ -10,63 +10,28 @@
  * @return string
  */
 function http_req($method, $url, $headers = array(), $data = '') {
-  $header = '';
-  foreach ($headers as $key => $value) {
-    $header .= $key . ': ' . $value . "\r\n";
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_HEADER, FALSE);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); // Set curl to return the data instead of printing it to the browser.
+  curl_setopt($ch, CURLOPT_URL, $url);
+
+  if ($method == 'GET' || $method == 'get') {
+    curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
+  } else if ($method == 'POST' || $method == 'post') {
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+    if ($data) {
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
   }
 
-  $options = array(
-    'http' => array(
-      'method' => $method,
-      'header' => $header,
-      'content' => $data
-    )
-  );
-
-  $context = stream_context_create($options);
-
-  $response = file_get_contents($url, FALSE, $context);
-
-  return $response;
-}
-
-function http_req_open($method, $url, $headers = array(), $data = '') {
-  $header = '';
-  foreach ($headers as $key => $value) {
-    $header .= $key . ': ' . $value . "\r\n";
+  if (is_array($headers) && sizeof($headers) > 0) {
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
   }
 
-  $options = array(
-    'http' => array(
-      'method' => $method,
-      'header' => $header,
-      'content' => $data
-    )
-  );
+  $data = curl_exec($ch);
+  curl_close($ch);
 
-  $context = stream_context_create($options);
-
-  return fopen($url, 'r', FALSE, $context);
-}
-
-/**
- * Downloads the file/resource at $source_url to the file at $destination_path.
- *
- * @param string $source_url
- * @param string $destination_path
- */
-function http_file_download($source_url, $destination_path) {
-  file_put_contents($destination_path, http_req_open('GET', $source_url));
-}
-
-function http_file_upload($source_file, $destination_url) {
-  $curl = curl_init();
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS,
-      array(
-        'file' => '@' . $source_file
-      ));
-  curl_setopt($curl, CURLOPT_URL, $destination_url);
-  curl_exec($curl);
-  curl_close($curl);
+  return $data;
 }
