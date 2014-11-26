62e3de65f96c14ef1de5637373f55718e0d5ab17
Paul Vorbach <paul@vorba.ch>
2014-11-26 15:45:39 +0100

    Update view

--- drupal/admin_view.php
+++ drupal/admin_view.php
@@ -8,18 +8,17 @@ function _get_admin_view($is_master = FALSE) {
   $view->description = '';
   $view->tag = 'default';
   $view->base_table = 'node';
-  $view->human_name = t('@turntable Administration',
+  $view->human_name = t('@turntable Admin',
       array(
         '@turntable' => $term
       ));
   $view->core = 7;
   $view->api_version = '3.0';
-  $view->disabled = FALSE;
+  $view->disabled = FALSE; /* Edit this to true to make a default view disabled initially */
 
   /* Display: Master */
   $handler = $view->new_display('default', 'Master', 'default');
-  $handler->display->display_options['title'] = t(
-      '@turntable Content Administration',
+  $handler->display->display_options['title'] = t('@turntable Admin',
       array(
         '@turntable' => $term
       ));
@@ -35,12 +34,14 @@ function _get_admin_view($is_master = FALSE) {
   $handler->display->display_options['style_options']['columns'] = array(
     'title' => 'title',
     'path' => 'path',
-    'created' => 'created',
-    'status' => 'status',
+    'shared_state' => 'shared_state',
+    'last_sync' => 'last_sync',
+    'original_client_id' => 'original_client_id',
     'edit_node' => 'edit_node',
-    'delete_node' => 'delete_node'
+    'delete_node' => 'delete_node',
+    'edit_tt_settings' => 'edit_tt_settings'
   );
-  $handler->display->display_options['style_options']['default'] = 'created';
+  $handler->display->display_options['style_options']['default'] = 'last_sync';
   $handler->display->display_options['style_options']['info'] = array(
     'title' => array(
       'sortable' => 1,
@@ -54,14 +55,21 @@ function _get_admin_view($is_master = FALSE) {
       'separator' => '',
       'empty_column' => 0
     ),
-    'created' => array(
+    'shared_state' => array(
+      'sortable' => 1,
+      'default_sort_order' => 'asc',
+      'align' => '',
+      'separator' => '',
+      'empty_column' => 0
+    ),
+    'last_sync' => array(
       'sortable' => 1,
       'default_sort_order' => 'desc',
       'align' => '',
       'separator' => '',
       'empty_column' => 0
     ),
-    'status' => array(
+    'original_client_id' => array(
       'sortable' => 1,
       'default_sort_order' => 'asc',
       'align' => '',
@@ -77,6 +85,11 @@ function _get_admin_view($is_master = FALSE) {
       'align' => '',
       'separator' => '',
       'empty_column' => 0
+    ),
+    'edit_tt_settings' => array(
+      'align' => '',
+      'separator' => '',
+      'empty_column' => 0
     )
   );
   /* Field: Content: Title */
@@ -89,42 +102,37 @@ function _get_admin_view($is_master = FALSE) {
   $handler->display->display_options['fields']['path']['id'] = 'path';
   $handler->display->display_options['fields']['path']['table'] = 'node';
   $handler->display->display_options['fields']['path']['field'] = 'path';
-  /* Field: Content: Post date */
-  $handler->display->display_options['fields']['created']['id'] = 'created';
-  $handler->display->display_options['fields']['created']['table'] = 'node';
-  $handler->display->display_options['fields']['created']['field'] = 'created';
-  $handler->display->display_options['fields']['created']['date_format'] = 'long';
-  $handler->display->display_options['fields']['created']['second_date_format'] = 'long';
-  /* Field: Content: Published */
-  $handler->display->display_options['fields']['status']['id'] = 'status';
-  $handler->display->display_options['fields']['status']['table'] = 'node';
-  $handler->display->display_options['fields']['status']['field'] = 'status';
-  $handler->display->display_options['fields']['status']['not'] = 0;
+  /* Field: Shared Content: Shared state */
+  $handler->display->display_options['fields']['shared_state']['id'] = 'shared_state';
+  $handler->display->display_options['fields']['shared_state']['table'] = 'tt_client_node_shared';
+  $handler->display->display_options['fields']['shared_state']['field'] = 'shared_state';
+  /* Field: Shared Content: Origin */
+  $handler->display->display_options['fields']['original_client_id']['id'] = 'original_client_id';
+  $handler->display->display_options['fields']['original_client_id']['table'] = 'tt_client_node_shared';
+  $handler->display->display_options['fields']['original_client_id']['field'] = 'original_client_id';
+  /* Field: Shared Content: Last sync */
+  $handler->display->display_options['fields']['last_sync']['id'] = 'last_sync';
+  $handler->display->display_options['fields']['last_sync']['table'] = 'tt_client_node_shared';
+  $handler->display->display_options['fields']['last_sync']['field'] = 'last_sync';
   /* Field: Content: Edit link */
   $handler->display->display_options['fields']['edit_node']['id'] = 'edit_node';
   $handler->display->display_options['fields']['edit_node']['table'] = 'views_entity_node';
   $handler->display->display_options['fields']['edit_node']['field'] = 'edit_node';
+  $handler->display->display_options['fields']['edit_node']['label'] = 'Edit';
   /* Field: Content: Delete link */
   $handler->display->display_options['fields']['delete_node']['id'] = 'delete_node';
   $handler->display->display_options['fields']['delete_node']['table'] = 'views_entity_node';
   $handler->display->display_options['fields']['delete_node']['field'] = 'delete_node';
+  $handler->display->display_options['fields']['delete_node']['label'] = 'Delete';
+  /* Field: Shared Content: Turntable settings */
+  $handler->display->display_options['fields']['edit_tt_settings']['id'] = 'edit_tt_settings';
+  $handler->display->display_options['fields']['edit_tt_settings']['table'] = 'custom';
+  $handler->display->display_options['fields']['edit_tt_settings']['field'] = 'edit_tt_settings';
   /* Sort criterion: Content: Post date */
   $handler->display->display_options['sorts']['created']['id'] = 'created';
   $handler->display->display_options['sorts']['created']['table'] = 'node';
   $handler->display->display_options['sorts']['created']['field'] = 'created';
   $handler->display->display_options['sorts']['created']['order'] = 'DESC';
-  if ($is_master) {
-    /* Filter criterion: Content: Type */
-    $handler->display->display_options['filters']['type']['id'] = 'type';
-    $handler->display->display_options['filters']['type']['table'] = 'node';
-    $handler->display->display_options['filters']['type']['field'] = 'type';
-    $handler->display->display_options['filters']['type']['value'] = array(
-      'shared' => 'shared'
-    );
-  } else {
-    /* Content sharing filter. */
-    $handler->display->display_options['filters']['type']['handler'] = 'turntable_client_views_content_filter';
-  }
   /* Filter criterion: Search: Search Terms */
   $handler->display->display_options['filters']['keys']['id'] = 'keys';
   $handler->display->display_options['filters']['keys']['table'] = 'search_index';
@@ -139,10 +147,41 @@ function _get_admin_view($is_master = FALSE) {
     1 => 0,
     3 => 0
   );
+  /* Filter criterion: Shared Content: Shared state */
+  $handler->display->display_options['filters']['shared_state']['id'] = 'shared_state';
+  $handler->display->display_options['filters']['shared_state']['table'] = 'tt_client_node_shared';
+  $handler->display->display_options['filters']['shared_state']['field'] = 'shared_state';
+  $handler->display->display_options['filters']['shared_state']['exposed'] = TRUE;
+  $handler->display->display_options['filters']['shared_state']['expose']['operator_id'] = 'shared_state_op';
+  $handler->display->display_options['filters']['shared_state']['expose']['label'] = 'Shared state';
+  $handler->display->display_options['filters']['shared_state']['expose']['operator'] = 'shared_state_op';
+  $handler->display->display_options['filters']['shared_state']['expose']['identifier'] = 'shared_state';
+  $handler->display->display_options['filters']['shared_state']['expose']['multiple'] = TRUE;
+  $handler->display->display_options['filters']['shared_state']['is_grouped'] = TRUE;
+  $handler->display->display_options['filters']['shared_state']['group_info']['label'] = 'Shared state';
+  $handler->display->display_options['filters']['shared_state']['group_info']['identifier'] = 'shared_state';
+  $handler->display->display_options['filters']['shared_state']['group_info']['widget'] = 'radios';
+  $handler->display->display_options['filters']['shared_state']['group_info']['group_items'] = array(
+    1 => array(
+      'title' => 'Copies',
+      'operator' => '=',
+      'value' => '1'
+    ),
+    2 => array(
+      'title' => 'References',
+      'operator' => '=',
+      'value' => '2'
+    ),
+    3 => array(
+      'title' => 'Original nodes',
+      'operator' => '=',
+      'value' => '3'
+    )
+  );
 
   /* Display: Page */
   $handler = $view->new_display('page', 'Page', 'page');
-  $handler->display->display_options['path'] = 'admin/content/turntable-manager';
+  $handler->display->display_options['path'] = 'admin/content/turntable-admin';
   $handler->display->display_options['menu']['type'] = 'tab';
   $handler->display->display_options['menu']['title'] = t('@turntable Manager',
       array(
@@ -153,5 +192,6 @@ function _get_admin_view($is_master = FALSE) {
   $handler->display->display_options['menu']['name'] = 'management';
   $handler->display->display_options['menu']['context'] = 0;
   $handler->display->display_options['menu']['context_only_inline'] = 0;
+
   return $view;
 }
